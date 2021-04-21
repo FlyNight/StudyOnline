9f82a9393c3f18caa171fcb7f6cc70cbe2bb5e0f
FlyNight <18810066972m@sina.cn>
2021-04-21 23:00:59 +0800

    2020/04/21-2300

--- app/src/main/java/com/example/studyonline/LessonStartActivity.kt
+++ app/src/main/java/com/example/studyonline/LessonStartActivity.kt
@@ -1,31 +1,81 @@
 package com.example.studyonline
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.VideoView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.studyonline.data.bean.Identity
 import com.github.faucamp.simplertmp.RtmpHandler
+import net.ossrs.yasea.SrsCameraView
+import net.ossrs.yasea.SrsRecordHandler
+import net.ossrs.yasea.SrsEncodeHandler
+import net.ossrs.yasea.SrsPublisher
 import java.io.IOException
 import java.lang.IllegalArgumentException
 import java.lang.IllegalStateException
 import java.net.SocketException
 
 
-class LessonStartActivity : AppCompatActivity(), RtmpHandler.RtmpListener {
+class LessonStartActivity :
+    AppCompatActivity(),
+    RtmpHandler.RtmpListener,
+    SrsRecordHandler.SrsRecordListener,
+    SrsEncodeHandler.SrsEncodeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lesson_start)
+        requestPermission()
         if (MainActivity.userIdentity == Identity.TEACHER.IDENTITY) {
             initTeacherView()
         }
         else {
             initStudentView()
+
         }
     }
-    private fun initTeacherView() {
-    }
-    private fun initStudentView() {
 
+    private fun requestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE), 100)
+        }
+    }
+
+    private fun initTeacherView() {
+        val cameraView: SrsCameraView = findViewById(R.id.live_video)
+        val videoView: VideoView = findViewById(R.id.live_watch)
+        cameraView.visibility = View.VISIBLE
+        videoView.visibility = View.GONE
+        val srsPublisher = SrsPublisher(cameraView)
+        srsPublisher.setEncodeHandler(SrsEncodeHandler(this))
+        srsPublisher.setRtmpHandler(RtmpHandler(this))
+        srsPublisher.setRecordHandler(SrsRecordHandler(this))
+        srsPublisher.setPreviewResolution(640,480)
+        srsPublisher.setScreenOrientation(1)
+        srsPublisher.setOutputResolution(640,480)
+        srsPublisher.setVideoHDMode()
+        srsPublisher.startCamera()
+        srsPublisher.switchToHardEncoder()
+        srsPublisher.startPublish("rtmp://82.156.194.22:1935/live/livestream")
+    }
+
+
+    private fun initStudentView() {
+        val cameraView: SrsCameraView = findViewById(R.id.live_video)
+        val videoView: VideoView = findViewById(R.id.live_watch)
+        cameraView.visibility = View.GONE
+        videoView.visibility = View.VISIBLE
+        videoView.setVideoPath("rtmp://82.156.194.22/live/livestream")
     }
 
     override fun onRtmpConnecting(msg: String?) {
@@ -77,6 +127,42 @@ class LessonStartActivity : AppCompatActivity(), RtmpHandler.RtmpListener {
     }
 
     override fun onRtmpIllegalStateException(e: IllegalStateException?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRecordPause() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRecordResume() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRecordStarted(msg: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRecordFinished(msg: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRecordIllegalArgumentException(e: IllegalArgumentException?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRecordIOException(e: IOException?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNetworkWeak() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNetworkResume() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onEncodeIllegalArgumentException(e: IllegalArgumentException?) {
         TODO("Not yet implemented")
     }
 }
